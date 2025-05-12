3d40aa2081f92c37edb318c53398a3774c41f49e
NOTCD13 <doriancamp13@gmail.com>
2025-05-12 11:30:27 -0600

    fix report and flakyness

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,9 +2,13 @@ import { LoginMethods } from "./pages/login/login.methods"
 
 describe('template spec', () => {
   it('passes', () => {
+    const usuario ="random01";
+    const contrasena = "random01";
     cy.visit('https://www.demoblaze.com/')
     cy.get("a[data-target='#logInModal']").click()
-    LoginMethods.Login("username","password")
-    cy.wait(5000)
+    LoginMethods.Login(usuario,contrasena)
+    cy.get('a#nameofuser').should("contain.text",usuario)
+    // cy.wait(5000)
+    
   })
 })
