028acd69ceb533c32ff699a3ec280f190fa0aa0e
NOTCD13 <doriancamp13@gmail.com>
2025-05-12 10:33:53 -0600

    video_59

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,10 @@
+import { LoginMethods } from "./pages/login/login.methods"
+
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit('https://www.demoblaze.com/')
+    cy.get("a[data-target='#logInModal']").click()
+    LoginMethods.Login("username","password")
+    cy.wait(5000)
   })
 })
