d1f29c93b849172813e87c979e8863ef7d4c77b3
NOTCD13 <doriancamp13@gmail.com>
2025-05-13 08:17:34 -0600

    creacion de archivos del home page

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,14 +1,19 @@
+import { homeMethods } from "./pages/home/home.methods"
 import { LoginMethods } from "./pages/login/login.methods"
 
 describe('template spec', () => {
   it('passes', () => {
-    const usuario ="random01";
-    const contrasena = "random01";
+    // const usuario ="random01";
+    // const contrasena = "random01";
+    // cy.visit('https://www.demoblaze.com/')
+    // cy.get("a[data-target='#logInModal']").click()
+    // LoginMethods.Login(usuario,contrasena)
+    // cy.get('a#nameofuser').should("contain.text",usuario)
+    // // cy.wait(5000)
+
     cy.visit('https://www.demoblaze.com/')
-    cy.get("a[data-target='#logInModal']").click()
-    LoginMethods.Login(usuario,contrasena)
-    cy.get('a#nameofuser').should("contain.text",usuario)
-    // cy.wait(5000)
+    homeMethods.clickOnProductLink("Iphone 6 32gb")
+    cy.wait(5000)
     
   })
 })
