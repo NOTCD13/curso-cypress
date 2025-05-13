6d6a8b6daa179ddbaabe665e2b2fb348720d8a13
NOTCD13 <doriancamp13@gmail.com>
2025-05-13 08:23:56 -0600

    creacion de archivos de products-details page

---
+++ cypress/e2e/pages/product-details/product-details.methods.js
@@ -0,0 +1,7 @@
+import { ProductDetails } from "./product-details.elements"
+
+export class ProductdetailsMethods{
+    static clickOnAddToCart(){
+        ProductDetails.buttons.addToCart.click()
+    }
+}
