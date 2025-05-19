8414d9324f383d1be3fad1a1a40b9a798ec93e20
NOTCD13 <doriancamp13@gmail.com>
2025-05-19 08:23:35 -0600

    creacion de pagina common page

---
+++ cypress/e2e/pages/common-page/common-page.elements.js
@@ -0,0 +1,24 @@
+export class CommonPageElements{
+    static get topMenu(){
+        return {
+            get home(){
+                return cy.contains("a","Home");
+            },
+            get Contact(){
+                return cy.contains("a","Contact");
+            },
+              get AboutUS(){
+                return cy.contains("a","About us");
+            },
+              get Cart(){
+                return cy.contains("a","Cart");
+            },
+              get LogIn(){
+                return cy.contains("a","Log in");
+            },
+              get SignUp(){
+                return cy.contains("a","Sign up");
+            },
+        };
+    }
+}
---
+++ cypress/e2e/pages/common-page/common-page.methods.js
@@ -0,0 +1,27 @@
+import { CommonPageData } from "./common-page.data";
+import { CommonPageElements } from "./common-page.elements";
+
+export class CommonPageMethods{
+    static navigateToDemoBlaze(){
+        cy.visit(CommonPageData.url);
+    }
+    static clickOnHomeOption(){
+        CommonPageElements.topMenu.home.click();
+    }
+    static clickOnContactOption(){
+        CommonPageElements.topMenu.Contact.click();
+    }
+    static clickOnAboutOption(){
+        CommonPageElements.topMenu.home.click();
+    }
+    static clickOnAboutUsOption(){
+        CommonPageElements.topMenu.AboutUS.click();
+    }
+    static clickOnLogInOption(){
+        CommonPageElements.topMenu.LogIn.click();
+    }
+    static clickOnSignUpOption(){
+        CommonPageElements.topMenu.SignUp.click();
+    }
+
+}
